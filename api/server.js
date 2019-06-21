fc9b015df0830c218b30df0276e04ffe90336b76
Brandon-Pampuch <brandonpampuch@gmail.com>
2019-06-21 10:38:22 -0500

    add ability to get all games

--- api/server.js
+++ api/server.js
@@ -20,7 +20,15 @@ server.get('/games', (req, res) => {
     });
 });
 
-
+server.get('/games', (req, res) => {
+  Games.getAll()
+    .then(games => {
+      res.status(200).json(games);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 
 module.exports = server;
